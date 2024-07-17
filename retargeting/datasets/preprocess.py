e909a8368e82913e223ed1842b463f46bcceb3a0
Chrysostomos Chadjiminas <chadjiminas.chrysostomos@gmail.com>
2024-07-17 09:35:01 +0300

    add optional downsample_step

--- retargeting/datasets/preprocess.py
+++ retargeting/datasets/preprocess.py
@@ -4,7 +4,7 @@ import shutil
 import numpy as np
 import copy
 
-from typing import List
+from typing import List, Optional
 
 from datasets.bvh_parser import BVH_file
 from datasets.motion_dataset import MotionData
@@ -12,15 +12,18 @@ from option_parser import get_args, try_mkdir
 from argparse import ArgumentParser
 
 
-def collect_bvh(data_path, character, files):
+def collect_bvh(data_path: str, character: str, files: List[str], downsample_step: Optional[int] = None):
     print('begin {}'.format(character))
     motions = []
 
     for i, motion in enumerate(files):
-        if not os.path.exists(data_path + character + '/' + motion):
+        file_path = os.path.join(data_path, character, motion)
+        if not os.path.exists(file_path):
             continue
-        file = BVH_file(data_path + character + '/' + motion)
+        file = BVH_file(file_path)
         new_motion = file.to_tensor().permute((1, 0)).numpy()
+        if downsample_step is not None:
+            new_motion = new_motion[::downsample_step]
         motions.append(new_motion)
 
     save_file = data_path + character + '.npy'
@@ -55,12 +58,15 @@ def copy_std_bvh(data_path: str, character: str, files: List[str]):
     shutil.copy(arbitrary_bvh_file_src, reference_bvh_file_tgt)
 
 
-if __name__ == '__main__':
+def main():
     parser = ArgumentParser()
     parser.add_argument("--prefix", type=str, default='./datasets/Mixamo/',)
+    parser.add_argument("--downsample-step", type=int, default=None)
     args = parser.parse_args()
 
     prefix: str = args.prefix
+    downsample_step: Optional[int] = args.downsample_step
+
     characters = [f for f in os.listdir(prefix) if os.path.isdir(os.path.join(prefix, f))]
     if 'std_bvhs' in characters: characters.remove('std_bvhs')
     if 'mean_var' in characters: characters.remove('mean_var')
@@ -72,6 +78,10 @@ if __name__ == '__main__':
         data_path = os.path.join(prefix, character)
         files = sorted([f for f in os.listdir(data_path) if f.endswith(".bvh")])
 
-        collect_bvh(prefix, character, files)
+        collect_bvh(prefix, character, files, downsample_step)
         copy_std_bvh(prefix, character, files)
         write_statistics(character, f'{prefix}/mean_var/', prefix=prefix)
+
+
+if __name__ == '__main__':
+    main()
